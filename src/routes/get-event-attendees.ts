b053a033c38e9b8bbf91b2d4d6df0e4224a7ac72
Thays Casado <thayscasado@gmail.com>
2024-04-03 23:34:05 -0700

    ⚡️ 🎨  Add pagination to the get attendees and search

--- src/routes/get-event-attendees.ts
+++ src/routes/get-event-attendees.ts
@@ -11,18 +11,60 @@ export async function getEventAttendees(app: FastifyInstance) {
         params: z.object({
           eventId: z.string().uuid(),
         }),
+
+        querystring: z.object({
+          query: z.string().nullish(), // nullable vs nullish -> nullish allows null and undefined while nullable allows only null
+          pageIndex: z.string().nullish().default("0").transform(Number), // default to 0 if not provided
+        }),
         response: {
+          200: z.object({
+            attendees: z.array(
+              z.object({
+                id: z.string().uuid(),
+                name: z.string(),
+                email: z.string().email(),
+                createdAt: z.date(),
+                checkInAt: z.date().nullish(),
+              })
+            ),
+          }),
+          404: z.object({
+            message: z.string(),
+          }),
             
         },
       },
     },
     async (request, reply) => {
       const { eventId } = request.params;
+      const { pageIndex, query} = request.query;
 
       // Fetch all attendees for the event
       const attendees = await prisma.attendee.findMany({
-        where: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          createdAt: true,
+          checkIn: {
+            select: {
+              createdAt: true,
+            },
+          
+          },
+        },
+        where: query ? {
           eventId,
+          name: {
+            contains: query, // `iLike` is case-insensitive search
+          },
+        } : {
+          eventId,
+        },
+        take: 10,
+        skip: pageIndex * 10, // it will skip 10 records for each page
+        orderBy: {
+          createdAt: "desc",
         },
       });
 
@@ -35,7 +77,13 @@ export async function getEventAttendees(app: FastifyInstance) {
       }
 
       return reply.status(200).send({
-        attendees,
+        attendees: attendees.map((attendee) => ({
+          id: attendee.id,
+          name: attendee.name,
+          email: attendee.email,
+          createdAt: attendee.createdAt,
+          checkInAt: attendee.checkIn?.createdAt || null,
+        })),
       });
     }
   );
